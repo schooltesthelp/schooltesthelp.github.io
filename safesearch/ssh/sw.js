dc2a4c82b3cb24b1f0621051dc9a88d61b9e9f38
greatlygravitate <87620301+greatlygravitate@users.noreply.github.com>
2021-10-11 09:50:34 -0400

    Add files via upload

---
+++ safesearch/ssh/sw.js
@@ -0,0 +1,33 @@
+const cacheName = 'js13kPWA-v1';
+const appShellFiles = [
+  '/safesearch/',
+  '/safesearch/index.html',
+  '/safesearch/app.js',
+  '/safesearch/style.css',
+  '/safesearch/img/safesearch.png',
+];
+
+const contentToCache = appShellFiles;
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil((async () => {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+  });
+
+
+  self.addEventListener('fetch', (e) => {
+    e.respondWith((async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) { return r; }
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+  });
